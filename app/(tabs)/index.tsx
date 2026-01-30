25397f8072ad76d7e056f2046415b19fd8bf4f76
Sandeep <sandeepsandeepkannan16@gmail.com>
2026-01-30 15:12:31 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{backgroundColor:"red"}}>
+         <Text>     </Text>
+    </View>
+  );
+}
