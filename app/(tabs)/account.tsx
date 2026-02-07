2752e344a5fd95ef1e4563e35210374ae37b7d0c
Sandeep <sandeepsandeepkannan16@gmail.com>
2026-02-07 16:10:44 +0530

    made the bottom top bar

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from "expo-router";
 
 export default function AccountRedirect() {
-  return <Redirect href="/(notabs)/accountend" />;
+  return <Redirect href="/(nobottombar)/accountend" />;
 }
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,16 @@
-import { View ,Text} from "react-native";
+import Downloadbar from "@/components/Bottomsheet";
+import { useState } from "react";
+import { View ,Text, Button} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Explorepage(){
-    return <SafeAreaView>
-        <Text > Explore  page </Text>
-    </SafeAreaView>
+
+    const [topbardownload,settopbardownload]=useState( false)
+
+    return <SafeAreaView style={{flex:1}}>
+                 
+            <Button title="Open bar" onPress={()=>{settopbardownload(true)}}></Button>
+           {topbardownload && <Downloadbar onClose={()=>{settopbardownload(false)}} />}
+        </SafeAreaView>
 }
